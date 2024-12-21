3eb98a4e3f14636dbceb5516be7d5898da22c2df
Furkan Bulut <furkanbtng@gmail.com>
2024-12-21 13:57:03 +0300

    detailed pet model and views

--- PetSoLive.Web/migration_script.sh
+++ PetSoLive.Web/migration_script.sh
@@ -1,2 +1,2 @@
-dotnet ef migrations add Initial --project ../PetSoLive.Data --startup-project .
+dotnet ef migrations add AddoptionStatusRemoveFRomPet --project ../PetSoLive.Data --startup-project .
 dotnet ef database update
